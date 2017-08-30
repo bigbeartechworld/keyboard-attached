88e67298f45f4aa7ebb51c8e134effc1130de4a6
ChristopherHicks <git@idragonmail.com>
2017-08-30 13:00:49 -0500

    Move ./src/module.ts to ./keyboard-attached.ts

--- keyboard-attached.ts
+++ keyboard-attached.ts
@@ -1,4 +1,21 @@
-import { KeyboardAttachedModule } from "./src/module";
+import { NgModule, APP_INITIALIZER } from "@angular/core";
+import { IonicModule, Config } from "ionic-angular";
+
 import { AutosizeDirective } from "./src/autosize/autosize";
 import { ReactToKeyboardDirective } from "./src/react-to-keyboard/react-to-keyboard";
 import { ContentReactToKeyboardDirective } from "./src/content-react-to-keyboard/content-react-to-keyboard";
+
+@NgModule({
+  declarations: [
+    AutosizeDirective,
+    ReactToKeyboardDirective,
+    ContentReactToKeyboardDirective
+  ],
+  imports: [IonicModule],
+  exports: [
+    AutosizeDirective,
+    ReactToKeyboardDirective,
+    ContentReactToKeyboardDirective
+  ]
+})
+export class KeyboardAttachedModule {}
